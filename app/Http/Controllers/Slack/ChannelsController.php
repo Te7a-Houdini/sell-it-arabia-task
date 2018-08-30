b1027916bac4e7a89ec1aca21d2ab10e72bb347d
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-08-30 19:34:57 +0200

    Add index implementation for slack channels

--- app/Http/Controllers/Slack/ChannelsController.php
+++ app/Http/Controllers/Slack/ChannelsController.php
@@ -9,6 +9,12 @@ class ChannelsController extends Controller
 {
     public function index()
     {
-
+        $channels = app('slack')->channels->list();
+        
+        throw_unless($channels['ok'],new \Exception('Error Connecting with slack'));
+     
+        return view('slack.channels.index',[
+            'channles' => $channels['channels']
+        ]);
     }
 }
