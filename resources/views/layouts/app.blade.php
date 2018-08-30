ae2e62ea10938f7c959d0dc0803abf213c64c03f
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-08-30 20:31:43 +0200

    Add link to create channel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,16 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+
+                <div class="collapse navbar-collapse" >
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                                <li class="nav-item">
+                                    <a class=" btn btn-success" href="#">Create Channel</a>
+                                </li>
+                        </ul>
+                    </div>
             </div>
         </nav>
 
