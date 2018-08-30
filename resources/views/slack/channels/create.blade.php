77c5f01016b9c399227d52ba788b0b20571c642d
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-08-30 20:44:56 +0200

    Add form for creating channel

--- resources/views/slack/channels/create.blade.php
+++ resources/views/slack/channels/create.blade.php
@@ -1,1 +1,13 @@
 @extends('layouts.app')
+
+@section('content')
+<form>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Channel Name</label>
+        <input required type="text" class="form-control" placeholder="Enter Channel Name">
+    </div>
+ 
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+@endsection
