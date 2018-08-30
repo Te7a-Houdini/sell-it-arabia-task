870266c156602e485039ec49b7c214c531be58b5
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-08-30 23:52:33 +0200

    Add fetching of messages history for a specific channel

--- app/Http/Controllers/Slack/ChannelsChatController.php
+++ app/Http/Controllers/Slack/ChannelsChatController.php
@@ -20,25 +20,40 @@ class ChannelsChatController extends Controller
 
         throw_unless($channelHistory['ok'], new \Exception('Error Channel Not Found'));
 
-        return view('slack.channels.chat.index',[
-            'messages' => $this->parseMessages(collect($channelHistory['messages']),collect($users['members'])),
-        ]);    
+        return view('slack.channels.chat.index', [
+            'messages' => $this->parseMessages(collect($channelHistory['messages']), collect($users['members'])),
+        ]);
     }
 
-    private function parseMessages($messages,$members)
+    private function parseMessages($messages, $members)
     {
         //this is just a hardcoded of my id , and this must be replaced
         //with a dynamic id that retrieved from authenticated slack users
 
         $currentUserId = 'UCHLMJ3C0';
 
-        return $messages->map(function($message) use($currentUserId,$members){
-
+        return $messages->map(function ($message) use ($currentUserId,$members) {
             return [
                 'type' => $message['user'] == $currentUserId ? 'outgoing':'received',
                 'user' => $members->firstWhere('id', $message['user']),
-                'text' => $message['text']
+                'text' => $this->replaceMentionedMembersInText($message['text'], $members)
             ];
-        });
+        })
+        ->reverse();
+    }
+
+    private function replaceMentionedMembersInText($text, $members)
+    {
+        if (preg_match('/<@([A-Z])\w+>/', $text, $matches)) {
+            $removeStartingLessThan = explode('<@', $matches[0])[1];
+
+            $removeEndingGreaterThan = explode('>', $removeStartingLessThan);
+               
+            $mentiondUser =  $members->firstWhere('id', $removeEndingGreaterThan[0])['real_name'];
+
+            $text = str_replace($matches[0], $mentiondUser, $text);
+        }
+
+        return $text;
     }
 }
